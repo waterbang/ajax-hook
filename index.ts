b02adb65c98390dd1c440dfb16cc0f1183299829
waterbang <409071692@qq.com>
2022-06-22 15:34:49 +0800

    ✨ 增加拦截fetch的方法

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
 export { hook, unHook } from "./src/hook";
 export { proxy, unProxy } from "./src/proxy";
+
+// 拦截fetch
+const originFetch = fetch;
+Object.defineProperty(window, "fetch", {
+  configurable: true,
+  enumerable: true,
+  // writable: true,
+  get() {
+    return (url: URL | RequestInfo, init?: RequestInit | undefined) => {
+      console.log("originFetch:", url);
+      return Promise.resolve({
+        text: () => {
+          return Promise.resolve().then((res) => {
+            return url;
+          });
+        },
+      });
+      // return originFetch.apply(window, [url, init]);
+    };
+  },
+});
